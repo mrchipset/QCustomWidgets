0fbec7af019fc194b3ebad2e1f02400b1e366de2
X.F. Zhou <xfzhou233@gmail.com>
2020-12-13 23:19:34 +0800

    FEAT: init the project

---
+++ QCustomWidgets/Buttons/SlideButton.cc
@@ -0,0 +1,26 @@
+#include "SlideButton.h"
+
+SlideButton::SlideButton(QWidget *parent) : QAbstractButton(parent)
+{
+
+}
+
+bool SlideButton::hitButton(const QPoint &pos) const
+{
+    return true;
+}
+
+void SlideButton::checkStateSet()
+{
+
+}
+
+void SlideButton::nextCheckState()
+{
+
+}
+
+void SlideButton::paintEvent(QPaintEvent *e)
+{
+
+}
---
+++ QCustomWidgets/Buttons/SlideButton.h
@@ -0,0 +1,16 @@
+#ifndef SLIDEBUTTON_H
+#define SLIDEBUTTON_H
+
+#include <QWidget>
+
+class SlideButton : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SlideButton(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // SLIDEBUTTON_H
